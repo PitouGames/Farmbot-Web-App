632f7dc02c73575ef19807bba1a049429519d6ed
Pitou <pitou.games@gmail.com>
2020-02-28 10:45:29 +0100

    Update docker-compose in ubuntu_example.sh

--- ubuntu_example.sh
+++ ubuntu_example.sh
@@ -22,8 +22,8 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update --yes
 sudo apt-get install docker-ce --yes
 sudo docker run hello-world # Should run!
-# Install docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# Install docker-compose. You can go on https://docs.docker.com/compose/install/ to check last released version.
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # Install FarmBot Web App
